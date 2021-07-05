653364b34832c584c05f3c9ef5fdd9aa5c53b694
Francisco Queiroz <78411956+foqueiroz@users.noreply.github.com>
2021-07-05 14:46:43 +0100

    Update slider.js

--- public/vrfood/slider.js
+++ public/vrfood/slider.js
@@ -57,12 +57,9 @@ AFRAME.registerComponent('slider', {
     localPosition.copy(evt.detail.position);
     el.object3D.updateMatrixWorld();
     el.object3D.worldToLocal(localPosition);
-   
-    //Trying to grab
     if (localPosition.x < -halfWidth || localPosition.x > halfWidth) { return; }
     this.pickerEl.object3D.position.x = localPosition.x;
     evtDetail.value = (this.pickerEl.object3D.position.x + halfWidth) / this.data.width;
     this.el.emit('sliderchanged', evtDetail);
-    
   }
 });
